ed7c917e9c2b7cdf3a3487f51abd12fe82353630
David <mingchang716@gmail.com>
2012-10-07 22:56:18 -0500

    first commit for confluence plugin skeleton

---
+++ src/test/java/com/joypot/confluence/plugins/ExampleMacroTest.java
@@ -0,0 +1,15 @@
+package com.joypot.confluence.plugins;
+
+import org.junit.Test;
+
+/**
+ * Testing {@link com.joypot.confluence.plugins.ExampleMacro}
+ */
+public class ExampleMacroTest
+{
+    @Test
+    public void basic()
+    {
+        // add test here...
+    }
+}
